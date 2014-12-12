1856b88b7cf26b23905eace991f0933be6e7bc93
weiyipeng <weiyipeng@gmail.com>
2014-12-13 00:24:20 +0800

    initial version
    init

---
+++ leetcode/leetcode/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by 魏弋彭 on 12/13/14.
+//  Copyright (c) 2014 魏弋彭. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
